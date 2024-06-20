99b12b7d356178b1a6097744199019909dad9bdc
Kaio Vinícios <kvsgpro@outlook.com>
2024-06-20 10:12:54 -0300

    Adicionando opções de categoria e alterando atributo de foto em galeria/models.py.

--- AluraSpace/galeria/models.py
+++ AluraSpace/galeria/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+OPCOES_CATEGORIA = [
+    ('NEBULOSA', 'Nebulosa'),
+    ('GALÁXIA', 'Galáxia'),
+    ('ESTRELA', 'Estrela'),
+    ('PLANETA', 'Planeta')
+]
+
 class Fotografia(models.Model):
     nome = models.CharField(max_length=100, blank=False, null=False)
     legenda = models.CharField(max_length=150, blank=False, null=False)
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, blank=False, null=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return f'Fotografia [nome = {self.nome}]'
